b445fb9fc076d639c5a795e58f5318a01d5cd29f
satishbandi201 <bandisatish20@gmail.com>
2025-07-10 17:48:52 +0530

    adding subnet ids

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,15 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
   value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
 }
